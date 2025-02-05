e370adf5a13bec66e76a338747bdd6415c562351
Diego Vinícius <diego.cavalcante90@gmail.com>
2025-02-05 15:03:31 -0300

    Adicionando classe ANBU

---
+++ src/Intermediario/Anbu.java
@@ -0,0 +1,18 @@
+package Intermediario;
+
+final class Anbu {
+
+    String nome;
+    int idade;
+    String aldeia;
+
+    public void anbu() {
+        System.out.println("Eu sou um ANBU");
+    }
+
+    public Anbu(String nome, int idade, String aldeia) {
+        this.nome = nome;
+        this.idade = idade;
+        this.aldeia = aldeia;
+    }
+}
